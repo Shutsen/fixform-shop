19b0deb902cb2f8f8843277bf972690678cef5dc
Geert Van Campenhout <vancampenhoutgeert@gmail.com>
2024-01-14 09:45:42 +0100

    base shop layout

---
+++ resources/js/Components/Products/Product.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps(["product"]);
+</script>
+
+<template>
+    <div class="group relative">
+        <div
+            class="aspect-h-3 aspect-w-2 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80"
+        >
+            <img
+                :src="product.image"
+                :alt="product.name"
+                class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+        </div>
+        <div class="mt-4 flex justify-between">
+            <div>
+                <h3 class="text-sm text-gray-700">
+                    <a href="/products">
+                        <span aria-hidden="true" class="absolute inset-0" />
+                        {{ product.name }}
+                    </a>
+                </h3>
+            </div>
+            <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import Product from "@/Components/Products/Product.vue";
+import { Head } from "@inertiajs/vue3";
+
+defineProps(["products"]);
+</script>
+
+<template>
+    <Head title="Products" />
+
+    <AuthenticatedLayout>
+        <div
+            class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+        >
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+                Products
+            </h2>
+            <div
+                class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+            >
+                <Product
+                    v-for="product in products"
+                    :key="product.id"
+                    :product="product"
+                />
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
